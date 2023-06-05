f951c19834a1b46e6603fa6bd18501b8f2131ba0
matulaz <matiaszarate.psi@gmail.com>
2023-06-05 11:04:23 -0300

    hola

---
+++ app.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import morgan from 'morgan'
+import mongoose from 'mongoose'
+import bodyParser from 'body-parser'
+
+
+
+const app= express()
+
+const producto={
+    nombre:"Iron Man",
+    img:"imagen",
+    precio:"37.95",
+    descripcion: "The most amazing comic from all over the years",
+    new: true,
+    exclusive: true,
+    bestseller: true,
+}
+
+app.set('view engine', 'ejs')
+app.use(express.static('public')) 
+
+app.get('/', (req,res)=>{
+    res.render('pages/index', {producto:producto}) //si aca pedimos info externa como una API usamos el asyn/await/try/catch
+})
+
+app.listen(3032, ()=>{
+    console.log("el servidor se esta ejecutando")
+})
+
